3f5d8b3d113997a25e45012e20e1f670739fc8c4
Parker TenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2022-07-25 15:49:24 -0400

    added basic rendering and sizing of terminal tab

--- app/src/tabs/terminal.rs
+++ app/src/tabs/terminal.rs
@@ -1,4 +1,4 @@
-use eframe::{epaint::{text::{LayoutJob}, FontId, Color32, Rounding}, egui::{TextFormat, ScrollArea}, emath::Vec2};
+use eframe::{epaint::{text::{LayoutJob}, FontId, Color32, Rounding, Stroke, self}, egui::{TextFormat, ScrollArea, Sense, TextEdit}};
 
 use super::tabbed_area::Tab;
 
@@ -19,12 +19,17 @@ impl TerminalTab{
 impl Tab for TerminalTab{
     fn ui(&mut self, ui: &mut eframe::egui::Ui) {
         let mut layout = LayoutJob::default();
-        let mut size = (ui.max_rect().width() * 1.84 / 80.0) as i32;
+        
+        let mut size = (ui.max_rect().width() * 1.8 / 80.0) as i32;
         if size <= 0 {
             size = 1;
         };
-        if size & 1 == 1{
-            size += 1;
+        while size > 1{
+            if FontId::monospace(size as f32).size * 24.0 > ui.available_height(){
+                size -= 1;
+            }else{
+                break;
+            }
         }
         let size = size as f32;
 
@@ -50,54 +55,65 @@ impl Tab for TerminalTab{
                 });
             }
             if i == 23{
+                let mut string = String::new();
                 for _ in 0..(80 - line.chars().count()){
-                    layout.append(" ", 0.0, TextFormat::default());
+                    string.push(' ');
                 }
+                layout.append(string.as_str(), 0.0, TextFormat{
+                    font_id: FontId::monospace(size),
+                    ..Default::default()
+                });
             }else{
-                layout.append("\n", 0.0, TextFormat::default());
+                layout.append("\n", 0.0, TextFormat{
+                    font_id: FontId::monospace(size),
+                    ..Default::default()
+                });
             }
         }
-        // layout.append("this is a test", 0.0, TextFormat{
-        //     font_id: FontId::monospace(size),
-        //     color: Color32::from_rgb(255,0,0),
-        //     background: Color32::from_rgb(255,255,0),
-        //     ..Default::default()
-        // });
-        // layout.append("01234567890123456789012345678901234567890123456789012345678901234567890123456789", 0.0, TextFormat{
-        //     font_id: FontId::monospace(size),
-        //     color: Color32::from_rgb(255,0,0),
-        //     background: Color32::from_rgb(0,255,0),
-        //     ..Default::default()
-        // });
-        // layout.append("\n01234567890123456789012345678901234567890123456789012345678901234567890123456789", 0.0, TextFormat{
-        //     font_id: FontId::monospace(size),
-        //     color: Color32::from_rgb(255,0,255),
-        //     ..Default::default()
-        // });
-        // layout.wrap = TextWrapping{
-        //     max_rows: 24,
-        //     ..Default::default()
-        // };
         
         let gallery = ui.fonts().layout_job(layout);
+        
+        let y_space = (ui.available_height() - gallery.rect.height()) / 2.0;
+        if y_space > 0.0{
+            ui.add_space(y_space);
+        }
 
-        let y = ui.next_widget_position().to_vec2().y;
         ui.horizontal(|ui|{
 
-            ui.add_space((ui.max_rect().width() - gallery.rect.width()) / 2.0);
+            let x_space = (ui.available_width() - gallery.rect.width()) / 2.0;
+            if x_space > 0.0{
+                ui.add_space(x_space);
+            }
             
+            let (pos, response) = ui.allocate_exact_size(gallery.size(), Sense{ click: true, drag: true, focusable: true });
+            if response.hovered() && ui.input().pointer.any_pressed(){
+                ui.memory().request_focus(response.id);
+            }
+            if ui.memory().has_focus(response.id){
+                ui.memory().lock_focus(response.id, true);
+
+            }
+
+            if ui.is_rect_visible(response.rect) {
+                
+                let stroke = if response.has_focus(){
+                    Stroke::new(5.0, Color32::RED)
+                }else{
+                    ui.style().visuals.widgets.noninteractive.bg_stroke
+                };
+                ui.painter().rect(pos.expand(2.0), 
+                                        Rounding::same(5.0), 
+                                        ui.style().visuals.extreme_bg_color, 
+                                        stroke);
             
-            let rect = gallery.rect;
-            let rect = rect.translate(Vec2::new(ui.next_widget_position().to_vec2().x, y));
-            //let rect = rect.translate(ui.next_widget_position().to_vec2());
-            
-            let rect = rect.expand(2.0);
-            //let width = gallery.rect.width();
-            //log::debug!("term width {}, font size: {}", width, size);
-            ScrollArea::new([true, true]).max_height(ui.max_rect().height()).show(ui, |ui|{
-                ui.painter().rect(rect, Rounding::same(5.0), ui.style().visuals.extreme_bg_color, ui.style().visuals.widgets.noninteractive.bg_stroke);
-                ui.label(gallery);
-            });
+                ui.painter().add(epaint::TextShape {
+                    pos: pos.left_top(),
+                    galley: gallery,
+                    override_text_color: None,
+                    underline: Stroke::none(),
+                    angle: 0.0,
+                });
+            }
         });
 
 
